925ba4230b54e02263c2450826e2d716b05394e8
Hong Zhuang <zhuanghong2013@gmail.com>
2017-10-31 22:03:07 +0000

    binding socket ok

--- server.cpp
+++ server.cpp
@@ -3,12 +3,16 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 //Network header file
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+using namespace std;
 
 int main(int argc, char *argv[]){
     int status;
@@ -17,21 +21,25 @@ int main(int argc, char *argv[]){
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
     hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_DGRAM; // TCP stream sockets
-    if ((status = getaddrinfo(argv[1], argv[2] , &hints, &servinfo)) != 0) {
+    hints.ai_flags = AI_PASSIVE; // use my IP
+    if ((status = getaddrinfo(NULL, argv[1] , &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+        exit(0);
     }
     
     int servsock;
-    if(servsock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol) == -1){
+    if((servsock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1){
         perror("linstener: socket");
-        exit(1);
+        exit(0);
     }
+    cout << "Socket established." << endl;
     
-    if (bind(servsock, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-            close(servsock);
-            perror("listener: bind");
-            exit(1);
+    if ((bind(servsock, servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
+        close(servsock);
+        perror("listener: bind");
+        exit(0);
     }
+    cout << "Socket binded." << endl;
     
+    exit(1);
 }
