19298581e0f5b0aa9b2db32af4f676755b3880a4
Hong Zhuang <zhuanghong2013@gmail.com>
2017-10-31 20:48:29 +0000

    bian lv

--- server.cpp
+++ server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 //Network header file
 #include <sys/types.h>
@@ -8,3 +9,20 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+int main(char argc, char *argv[]){
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_DGRAM; // TCP stream sockets
+    if ((status = getaddrinfo(argv[1], argv[2] , &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    
+    
+    
+    
+}
