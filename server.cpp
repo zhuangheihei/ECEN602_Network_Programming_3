36fbc3542fc9cd6f2ff7548a2aff5178a73be0a2
Hong Zhuang <zhuanghong2013@gmail.com>
2017-11-02 20:36:56 +0000

    testing

--- server.cpp
+++ server.cpp
@@ -352,6 +352,7 @@ int main(int argc, char *argv[]){
                 }
                 resend_count ++;
             }//end of transfering packets
+            
             f.close();
             close(serv_sock);
             cout << "File transmission complete.\n" << endl;
